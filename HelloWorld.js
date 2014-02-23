cb2a45ae18b7ec3229333da96f1b869ec7bc5185
Michael Gillson <mike@michaelgillson.org>
2014-02-22 23:38:08 -0500

    Listen on 0.0.0.0 Now I understand.

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,11 +1,11 @@
 console.log("Hello World - First Line");
 
 var port = Number(process.env.PORT || 5000);
-var ip = process.env.IP;
+var ip = process.env.IP || '0.0.0.0';
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World on Port -' + port);
+    res.end('Hello World at - ' + ip + ':' + port);
 }).listen(port,ip);
 
 console.log('Server running at - ' + ip + ':' + port);
