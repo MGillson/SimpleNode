ad334f24bf6be9eca3f5db1a87d4fe50e7a7666d
Michael Gillson <mike@michaelgillson.org>
2014-02-22 20:36:32 -0500

    Figure out how ports and ip work in node

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,17 +1,11 @@
 console.log("Hello World - First Line");
 
-// set port, ip from command 
-// $ export PORT=3333
-// $ export IP=127.0.0.1
-// $ node app.js 
-// $ NODE_ENV=production node app.js
-// Hope null ip becomes localhost
 var port = Number(process.env.PORT || 5000);
-var ip = process.env.IP || '127.0.0.1';
+var ip = process.env.IP;
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World ' + ip + ':' + port);
+    res.end('Hello World on Port -' + port);
 }).listen(port,ip);
 
 console.log('Server running at - ' + ip + ':' + port);
